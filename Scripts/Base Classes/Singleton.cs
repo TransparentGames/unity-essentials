e7f9640289844d26ecc045df72256b3645064517
DuDuSteo <p.aleksiejuk@protonmail.com>
2023-11-08 00:04:07 +0100

    first commit

---
+++ Scripts/Base Classes/Singleton.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<T>();
+            }
+            return _instance;
+        }
+    }
+    public static bool IsInitialized => _instance != null;
+
+    private static T _instance;
+}
