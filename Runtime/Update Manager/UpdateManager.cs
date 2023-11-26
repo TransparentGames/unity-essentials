1bb23705ca89e21c5d0cf92418cda1292e719ecb
DuDuSteo <p.aleksiejuk@protonmail.com>
2023-11-26 20:17:32 +0100

    Refactor singleton classes

--- Runtime/Update Manager/UpdateManager.cs
+++ Runtime/Update Manager/UpdateManager.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using UnityEngine;
+using Singletons;
 using UnityEngine.Assertions;
 
 public class UpdateManager : PersistentMonoSingleton<UpdateManager>
