6e7f4d898a2e38b743914d13ffd6fbef578ce256
DuDuSteo <p.aleksiejuk@protonmail.com>
2024-03-13 17:52:59 +0100

    Add playfab definition for login

---
+++ Runtime/PlayFab/ClientPlayFabHandler.cs
@@ -0,0 +1,96 @@
+using System;
+using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine;
+
+namespace TransparentGames.Essentials.PlayFab
+{
+    public static class ClientPlayFabHandler
+    {
+        public static string SessionTicket { get; private set; }
+
+        #region Register
+        public static void Register(string email, string username, string password,
+        Action<RegisterPlayFabUserResult> successCallback)
+        {
+            PlayFabClientAPI.RegisterPlayFabUser(new RegisterPlayFabUserRequest
+            {
+                Email = email,
+                Username = username,
+                Password = password,
+                RequireBothUsernameAndEmail = false
+            }, successResult =>
+            {
+                // TODO: Move this to a separate UI
+                CreateCharacter(username, (result) =>
+                {
+                    Debug.Log($"Successfully registered [{username}] user");
+                    successCallback(successResult);
+                });
+            }, PlayFabFailure);
+        }
+        #endregion
+
+        #region Login
+
+        public static void Login(string username, string password,
+        Action<LoginResult> successCallback)
+        {
+            PlayFabClientAPI.LoginWithPlayFab(new LoginWithPlayFabRequest
+            {
+                Username = username,
+                Password = password,
+                InfoRequestParameters = new GetPlayerCombinedInfoRequestParams
+                {
+                    GetPlayerProfile = true,
+                }
+            }, successResult =>
+            {
+                SessionTicket = successResult.SessionTicket;
+                Debug.Log($"Successfully logged with [{SessionTicket}] user");
+                successCallback(successResult);
+            }, PlayFabFailure);
+        }
+
+
+
+        #endregion
+
+        public static void CreateCharacter(string displayName,
+        Action<UpdateUserDataResult> successCallback)
+        {
+            PlayFabClientAPI.UpdateUserData(new UpdateUserDataRequest
+            {
+                Data = new System.Collections.Generic.Dictionary<string, string>
+            {
+                { "display_name", displayName }
+            }
+            }, successResult =>
+            {
+                Debug.Log($"Successfully created [{displayName}] character");
+                successCallback(successResult);
+            }, PlayFabFailure);
+        }
+
+        public static void GetPlayerProfile(Action<GetPlayerProfileResult> successCallback)
+        {
+            PlayFabClientAPI.GetPlayerProfile(new GetPlayerProfileRequest
+            {
+                ProfileConstraints = new PlayerProfileViewConstraints
+                {
+                    ShowDisplayName = true
+                }
+            }, successResult =>
+            {
+                Debug.Log($"Successfully retrieved [{successResult.PlayerProfile.DisplayName}] player profile");
+                successCallback(successResult);
+            }, PlayFabFailure);
+
+        }
+
+        private static void PlayFabFailure(PlayFabError error)
+        {
+            Debug.Log(error.GenerateErrorReport());
+        }
+    }
+}
