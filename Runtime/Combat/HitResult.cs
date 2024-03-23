501078687c0ea465e825882daac3508a06110747
Roshiroku <roshirokuch@gmail.com>
2024-03-23 14:50:16 +0100

    Add more combat core scripts

---
+++ Runtime/Combat/HitResult.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace TransparentGames.Essentials.Combat
+{
+    public struct HitResult
+    {
+        public int damageDealt;
+        public bool wasKilled;
+        public GameObject hitObject;
+    }
+}
