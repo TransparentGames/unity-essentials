29cd33e8b121140fa1a6a7950ea96f07dea53f60
DuDuSteo <p.aleksiejuk@protonmail.com>
2024-04-14 22:16:12 +0200

    Changed name of hittable interface, added layer mask for detection

--- Runtime/Combat/IHittable.cs
+++ Runtime/Combat/IHittable.cs
@@ -6,7 +6,7 @@ namespace TransparentGames.Essentials.Combat
     public interface IHittable
     {
         public GameObject GameObject { get; }
-        public event Action<HitResult> HitEvent;
+        public event Action<HitResult> HitResultEvent;
         public event Action<HitInfo> HitInfoEvent;
 
         public HitResult OnHit(HitInfo info);
