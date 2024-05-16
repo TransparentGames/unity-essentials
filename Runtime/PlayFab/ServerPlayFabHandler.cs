6e47d31efb7a3086c01544f9b0d6828b3256ae06
Roshiroku <roshirokuch@gmail.com>
2024-05-16 23:53:30 +0200

    Attempt at updating way of handling client and server

--- Runtime/PlayFab/ServerPlayFabHandler.cs
+++ Runtime/PlayFab/ServerPlayFabHandler.cs
@@ -38,9 +38,67 @@ namespace TransparentGames.Essentials.PlayFab
                 Data = data
             }, successResult =>
             {
-                successCallback(successResult);
                 Debug.Log("Successfully updated user data");
+                successCallback(successResult);
             }, PlayFabFailure);
+        }
+
+        public static void GetItemsFromDropTable(string tableId, string playFabId, Action<GrantItemsToUserResult> successCallback, Action<PlayFabError> errorCallback = null)
+        {
+            PlayFabServerAPI.GetRandomResultTables(new GetRandomResultTablesRequest()
+            {
+                TableIDs = new List<string> { tableId }
+            }, successResult =>
+            {
+                Debug.Log("Completed getting drop tables");
+                EvaluateRandomResultTable(tableId, playFabId, successCallback, errorCallback);
+            }, errorCallback ?? PlayFabFailure);
+        }
+
+        public static void ReadDropTableData(string tableId, Action<GetRandomResultTablesResult> successCallback, Action<PlayFabError> errorCallback = null)
+        {
+            PlayFabServerAPI.GetRandomResultTables(new GetRandomResultTablesRequest()
+            {
+                TableIDs = new List<string> { tableId }
+            }, successResult =>
+            {
+                Debug.Log("Completed getting drop tables");
+                successCallback(successResult);
+            }, errorCallback ?? PlayFabFailure);
+        }
+
+        public static void EvaluateRandomResultTable(string tableId, string playFabId, Action<GrantItemsToUserResult> successCallback, Action<PlayFabError> errorCallback = null)
+        {
+            PlayFabServerAPI.EvaluateRandomResultTable(new EvaluateRandomResultTableRequest
+            {
+                TableId = tableId,
+            },
+            successResult =>
+            {
+                Debug.Log("Successfully got random result tables");
+                OnRandomResultTableResponse(successResult, playFabId, successCallback, errorCallback);
+            }, errorCallback ?? PlayFabFailure);
+        }
+
+        private static void OnRandomResultTableResponse(EvaluateRandomResultTableResult tableResult, string playFabId, Action<GrantItemsToUserResult> successCallback, Action<PlayFabError> errorCallback = null)
+        {
+            // First, check if the result is valid
+            if (tableResult.ResultItemId == null)
+            {
+                Debug.LogError("Invalid result item id");
+                return;
+            }
+
+            // Second, take the result and grant it to the player
+            PlayFabServerAPI.GrantItemsToUser(new GrantItemsToUserRequest
+            {
+                PlayFabId = playFabId,
+                ItemIds = new List<string> { tableResult.ResultItemId }
+            }, successResult =>
+            {
+                successCallback(successResult);
+                // Handle Result
+            }, errorCallback ?? PlayFabFailure);
         }
 
         private static void PlayFabFailure(PlayFabError error)
