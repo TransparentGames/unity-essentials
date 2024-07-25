a54ac1945e2e24f9653fe1617988b090b01e11f1
DuDuSteo <p.aleksiejuk@protonmail.com>
2024-07-25 16:21:16 +0200

    Update a bit way of handling caster

--- Runtime/Abilities/Caster.cs
+++ Runtime/Abilities/Caster.cs
@@ -1,112 +1,23 @@
-using UnityEngine;
-using TransparentGames.Essentials.Stats;
-using System.Collections.Generic;
-using TransparentGames.Essentials;
-using TransparentGames.Essentials.Combat;
-using System;
 
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
+using System;
+using UnityEngine;
 
 namespace TransparentGames.Essentials.Abilities
 {
-    public class Caster : MonoBehaviour, IStatsRequired, IComponent
+    public abstract class Caster : MonoBehaviour, IComponent
     {
-        public event Action Ready;
+        public virtual event Action Ready;
         public GameObject Owner { get; set; }
-        public Animator Animator => Owner.GetComponentInChildren<Animator>();
-        public bool IsBusy => _ability != null && _ability.CanCancel == false;
 
-        [SerializeField] private AbilityTemplate abilityTemplate;
+        public abstract Animator Animator { get; }
+        public abstract bool IsReady { get; }
 
-        private Ability _ability = null;
+        public abstract void Cast(GameObject target = null);
+        public abstract bool CanCast();
 
-        private StatsHolder _statsHolder;
-
-        private void OnDisable()
+        protected void OnReady()
         {
-            if (_ability != null)
-            {
-                Abort();
-            }
-        }
-
-        public bool CanCast()
-        {
-            if (abilityTemplate == null)
-                return false;
-
-            if (abilityTemplate.CanUse(this) == false)
-                return false;
-
-            return _ability == null || _ability.CanCancel;
-        }
-
-        public void Cast(GameObject target = null)
-        {
-            if (_ability)
-                Abort();
-
-            _ability = Instantiate(abilityTemplate.abilityPrefab, Owner.transform.position, Owner.transform.rotation);
-
-            var hitInfo = abilityTemplate.Calculate(_statsHolder.Stats);
-            hitInfo.source = Owner;
-            hitInfo.level = _statsHolder.Level;
-
-            _ability.HitInfo = hitInfo;
-            _ability.LayerMask = abilityTemplate.layerMask;
-            _ability.HitResultsEvent += OnHitResults;
-            _ability.Finished += OnAbilityFinished;
-
-            _ability.Use(this);
-        }
-
-        public void OnStatsChanged(StatsHolder statsHolder)
-        {
-            _statsHolder = statsHolder;
-        }
-
-        private void OnHitResults(List<HitResult> hitResults)
-        {
-            foreach (HitResult hitResult in hitResults)
-            {
-
-            }
-        }
-
-        private void OnAbilityFinished()
-        {
-            _ability.Finished -= OnAbilityFinished;
-            Abort();
             Ready?.Invoke();
         }
-
-        private void Abort()
-        {
-            Destroy(_ability.gameObject);
-            _ability = null;
-        }
     }
-
-#if UNITY_EDITOR
-    [CustomEditor(typeof(Caster), true)]
-    public class CasterEditor : Editor
-    {
-        public override void OnInspectorGUI()
-        {
-            base.OnInspectorGUI();
-
-            GUILayout.Space(20);
-
-            if (target is not Caster caster)
-                return;
-
-            EditorGUILayout.LabelField("Editor", EditorStyles.boldLabel);
-
-            if (GUILayout.Button("Cast"))
-                caster.Cast();
-        }
-    }
-#endif
 }
