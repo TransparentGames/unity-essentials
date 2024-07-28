09c1c3abd3fb906ff672430ce2080c50df81d4df
Roshiroku <roshirokuch@gmail.com>
2024-07-28 22:53:53 +0200

    Add name for item template

--- Runtime/Items/ItemTemplate.cs
+++ Runtime/Items/ItemTemplate.cs
@@ -5,8 +5,9 @@ namespace TransparentGames.Essentials.Items
     [CreateAssetMenu(fileName = "New Item Template", menuName = "Transparent Games/Items/Item Template", order = 0)]
     public class ItemTemplate : PlayfabItemTemplate
     {
+        public virtual string Name => itemName;
         [Space]
         public Sprite icon;
-        public string itemName;
+        [SerializeField] protected string itemName;
     }
 }
