a448510eca1e4d7f5ffbade56ce2bd6f6a3f8c07
Roshiroku <roshirokuch@gmail.com>
2024-07-29 10:21:53 +0200

    Add detectable object that uses IHealth and unregisters on death

---
+++ Runtime/Detection/HealthDetectableObject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using TransparentGames.Essentials.Combat;
+using UnityEngine;
+
+namespace TransparentGames.Essentials.Detection
+{
+    [RequireComponent(typeof(IHealth))]
+    public class HealthDetectableObject : DetectableObject
+    {
+        private void Awake()
+        {
+            GetComponent<IHealth>().ValueZeroed += OnHealthValueZeroed;
+        }
+
+        private void OnDestroy()
+        {
+            GetComponent<IHealth>().ValueZeroed -= OnHealthValueZeroed;
+        }
+
+        private void OnHealthValueZeroed()
+        {
+            DetectableRegistry.UnRegister(this);
+            OnDetectionChanged();
+        }
+    }
+}
