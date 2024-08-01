5829cc31d7272b53c77fef42aecb1690a5a962e9
Roshiroku <roshirokuch@gmail.com>
2024-08-01 15:47:40 +0200

    Update currency handling, migrate slowly to item handling

--- Runtime/Data/Nodes/Node.cs
+++ Runtime/Data/Nodes/Node.cs
@@ -8,7 +8,7 @@ namespace TransparentGames.Essentials.Data.Nodes
     /// <summary>
     /// Base class for getting the display value of a node
     /// </summary>
-    public abstract class Node : ItemTemplate
+    public abstract class Node : ScriptableObject
     {
         public abstract string DisplayValue { get; }
         public abstract void AddListener(Action callback);
