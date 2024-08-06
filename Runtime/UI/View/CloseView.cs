980af385ef8407e86869d11ca9f4ea097ee147f5
Roshiroku <roshirokuch@gmail.com>
2024-08-06 12:58:16 +0200

    Streamline the use of close view

---
+++ Runtime/UI/View/CloseView.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace TransparentGames.Essentials.UI
+{
+    [RequireComponent(typeof(View))]
+    public class CloseView : MonoBehaviour
+    {
+        [SerializeField] private List<InputActionReference> closeActions;
+
+        private View _baseView;
+
+        private void Awake()
+        {
+            _baseView = GetComponent<View>();
+        }
+
+        private void OnEnable()
+        {
+            foreach (var closeAction in closeActions)
+            {
+                closeAction.action.Enable();
+                closeAction.action.performed += ctx => _baseView.TryClose();
+            }
+        }
+
+        private void OnDisable()
+        {
+            foreach (var closeAction in closeActions)
+            {
+                closeAction.action.performed -= ctx => _baseView.TryClose();
+                closeAction.action.Disable();
+            }
+        }
+    }
+}
