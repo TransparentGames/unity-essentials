ead60b88b875ece6c9f63fe7e94cbd1a74847f02
DuDuSteo <p.aleksiejuk@protonmail.com>
2024-09-18 12:14:26 +0200

    Handle ability templates as items

--- Runtime/Items/ItemTemplate.cs
+++ Runtime/Items/ItemTemplate.cs
@@ -5,13 +5,22 @@ namespace TransparentGames.Essentials.Items
     [CreateAssetMenu(fileName = "New Item Template", menuName = "Transparent Games/Items/Item Template", order = 0)]
     public class ItemTemplate : PlayfabItemTemplate
     {
+        /// <summary>
+        /// The name of the item.
+        /// </summary>
         public virtual string Name => itemName;
         public virtual string Description => description;
         [Space]
+        /// <summary>
+        /// The icon of the item.
+        /// </summary>
         public Sprite icon;
+        [Tooltip("Used for categorizing items")] public string itemClass;
+        /// <summary>
+        /// The maximum amount of this item that can be stacked in a single inventory slot.
+        /// </summary>
+        public bool IsUnique;
         [SerializeField] protected string itemName;
         [SerializeField, TextArea] protected string description;
-        public string itemClass;
-        public bool IsUnique;
     }
 }
