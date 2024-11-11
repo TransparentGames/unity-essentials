0c50a08b5a43d8f551556a609132895efa381d21
Roshiroku <roshirokuch@gmail.com>
2024-11-11 23:13:53 +0100

    Streamline cached data manager

--- Runtime/Data/IDataSaveManager.cs
+++ Runtime/Data/IDataSaveManager.cs
@@ -2,8 +2,6 @@ namespace TransparentGames.Essentials.Data
 {
     public interface IDataSaveManager
     {
-        public IDataProperty<int> GetProperty(string key, int defaultValue);
-        public IDataProperty<bool> GetProperty(string key, bool defaultValue);
-        public IDataProperty<float> GetProperty(string key, float defaultValue);
+        public IDataProperty<T> GetProperty<T>(string key, T defaultValue);
     }
 }
